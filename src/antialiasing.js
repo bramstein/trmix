49f2cc64f60e69f49f1fb13ed297603e35e2fe42
Bram Stein <stein@adobe.com>
2013-11-19 18:42:07 +0100

    Add method for guessing the antialiasing method.

--- src/antialiasing.js
+++ src/antialiasing.js
@@ -51,7 +51,36 @@ goog.scope(function () {
     if (antialiasing !== Antialiasing.UNKNOWN) {
       return antialiasing;
     } else {
-      return Antialiasing.UNKNOWN;
+      if (userAgent.getPlatform() === Platform.OSX ||
+          userAgent.getPlatform() === Platform.LINUX) {
+        // Font-smoothing is turned on by default on OSX, so
+        // we assume most people do not disable it.
+        return Antialiasing.SUBPIXEL;
+      } else if (userAgent.getPlatform() === Platform.WINDOWS) {
+        // Windows Vista+
+        if (userAgent.getPlatformVersion().ge(new Version(6, 0))) {
+          // ClearType is turned on by default on Windows Vista+ and
+          // has improved a lot with DirectWrite, so we assume most
+          // people have turned it on.
+          return Antialiasing.SUBPIXEL;
+        } else {
+          if (userAgent.getBrowser() === Browser.INTERNET_EXPLORER) {
+            if (userAgent.getBrowserVersion().ge(new Version(7, 0))) {
+              return Antialiasing.SUBPIXEL;
+            } else {
+              // IE6 most likely does not have ClearType turned on
+              return Antialiasing.GRAYSCALE;
+            }
+          } else {
+            // If the user is using modern browsers on WindowsXP, they
+            // most likely also have IE7 or IE8 installed and thus
+            // ClearType enabled.
+            return Antialiasing.SUBPIXEL;
+          }
+        }
+      } else {
+        return Antialiasing.UNKNOWN;
+      }
     }
   };
 });
--- src/tr.js
+++ src/tr.js
@@ -7,7 +7,14 @@ goog.require('tr.dom');
 
 var userAgent = tr.UserAgent.parse(goog.global.navigator.userAgent),
     rasterizer = tr.Rasterizer.get(userAgent),
-    antialiasing = tr.Antialiasing.get(userAgent);
+    antialiasing = tr.Antialiasing.get(userAgent),
+    antialiasingGuess = tr.Antialiasing.guess(userAgent);
 
 tr.dom.addClass(goog.global.document.documentElement, 'tr-' + rasterizer);
+
+if (antialiasing === tr.Antialiasing.UNKNOWN && antialiasingGuess !== tr.Antialiasing.UNKNOWN) {
+  antialiasing = antialiasingGuess;
+  tr.dom.addClass(goog.global.document.documentElement, 'tr-aa-unknown');
+}
+
 tr.dom.addClass(goog.global.document.documentElement, 'tr-aa-' + antialiasing);
