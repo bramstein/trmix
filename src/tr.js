80f1c61fd48292cdbbde61af7f88c57f00ef75e4
Bram Stein <stein@adobe.com>
2013-11-20 15:18:49 +0100

    Simplify code. Guessed antialiasing methods are now named 'tr-aa-unknown-<guess>'.

--- src/tr.js
+++ src/tr.js
@@ -8,12 +8,13 @@ goog.require('tr.dom');
 var userAgent = tr.UserAgent.parse(goog.global.navigator.userAgent),
     rasterizer = tr.Rasterizer.get(userAgent),
     antialiasing = tr.Antialiasing.get(userAgent),
-    antialiasingGuess = tr.Antialiasing.guess(userAgent);
+    antialiasingGuess = tr.Antialiasing.guess(userAgent),
+    documentElement = goog.global.document.documentElement;
 
-tr.dom.addClass(goog.global.document.documentElement, 'tr-' + rasterizer);
+tr.dom.addClass(documentElement, 'tr-' + rasterizer);
 
 if (antialiasing === tr.Antialiasing.UNKNOWN && antialiasingGuess !== tr.Antialiasing.UNKNOWN) {
-  antialiasing = antialiasingGuess + '-' + tr.Antialiasing.UNKNOWN;
+  antialiasing += '-' + antialiasingGuess;
 }
 
-tr.dom.addClass(goog.global.document.documentElement, 'tr-aa-' + antialiasing);
+tr.dom.addClass(documentElement, 'tr-aa-' + antialiasing);
