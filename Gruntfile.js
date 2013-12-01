0c5720be5bf2432ae71ea05155dd9548c0b43dce
Bram Stein <stein@adobe.com>
2013-12-01 09:37:27 +0100

    Add automated testing setup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,8 +15,33 @@ module.exports = function (grunt) {
     exec: {
       deps: {
         command: 'calcdeps -i src -p src -p vendor/google/base.js -o deps > test/deps.js'
+      },
+      test: {
+        command: 'open test/index.html'
+      },
+      browserstacktest: {
+        command: 'node_modules/.bin/browserstack-test' +
+                 ' -u ' +
+                    (grunt.option('browserstack-username') || '$BROWSERSTACK_USERNAME') +
+                 ' -p ' +
+                    (grunt.option('browserstack-password') || '$BROWSERSTACK_PASSWORD') +
+                 ' -k ' +
+                    (grunt.option('browserstack-key') || '$BROWSERSTACK_KEY') +
+                 ' -b browsers.json -t 120 http://localhost:9999/test/index.html'
+      },
+      localtunnel: {
+        command: 'node_modules/.bin/lt --port 9999'
       }
     },
+    connect: {
+      server: {
+        options: {
+          base: "",
+          port: 9999
+        }
+      }
+    },
+    watch: {},
     closurecompiler: {
       compile: {
         files: {
@@ -39,11 +64,15 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks('grunt-closurecompiler');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-exec');
 
   grunt.registerTask('compile', ['closurecompiler:compile']);
   grunt.registerTask('deps', ['exec:deps']);
   grunt.registerTask('debug', ['closurecompiler:debug']);
+  grunt.registerTask('browsertest', ['connect', 'exec:browserstacktest']);
+  grunt.registerTask('localtunnel', ['connect', 'exec:localtunnel']);
+  grunt.registerTask('test', ['exec:test']);
   grunt.registerTask('default', ['compile']);
 };
