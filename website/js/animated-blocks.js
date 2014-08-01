2766e6ef59334261967948de24ca7e934643e3e0
Bram Stein <stein@adobe.com>
2014-08-01 11:12:52 +0200

    Tweak header.

--- website/js/animated-blocks.js
+++ website/js/animated-blocks.js
@@ -1,10 +1,15 @@
 (function () {
   var container = document.getElementById('block-container');
-  var columns = 16;
-  var rows = 10;
-  var cd = {firstx : 2, firsty : 2, lastx : columns - 3, lasty : rows - 2};
+  var columns = 20;
+  var rows = 8;
+  var cd = {
+    xstart : 2,
+    ystart : 1,
+    xend : columns - 3,
+    yend : rows - 2
+  };
   var delay = [];
-  var animatedblocks = rows * columns - (cd.lastx - cd.firstx) * (cd.lasty - cd.firsty);
+  var animatedblocks = rows * columns - (cd.xend - cd.xstart) * (cd.yend - cd.ystart);
 
   for(var i = 0; i <  animatedblocks; i++){
     delay.push(i / 2);
@@ -13,13 +18,12 @@
   for(var y = 0; y < rows; y++){
     for(var x = 0; x < columns; x++) {
       var block = document.createElement('div');
-      if (!(x >= cd.firstx && x <= cd.lastx && y >= cd.firsty && y < cd.lasty)){
+      if (!(x >= cd.xstart && x <= cd.xend && y >= cd.ystart && y <= cd.yend)){
         var randomindex = Math.floor(Math.random() * (delay.length));
-        block.style.cssText =
-          'animation : animate-opacity 20s ' +  delay[randomindex] + 's infinite;' +
-          '-webkit-animation : animate-opacity 20s ' +  delay[randomindex] + 's infinite;' +
-          '-moz-animation : animate-opacity 20s ' +  delay[randomindex] + 's infinite;' +
-          '-o-animation : animate-opacity 20s ' +  delay[randomindex] + 's infinite;';
+        block.style.cssText = 'animation : animate-opacity 20s ' +  delay[randomindex] + 's infinite;' +
+                              '-webkit-animation : animate-opacity 20s ' +  delay[randomindex] + 's infinite;' +
+                              '-moz-animation : animate-opacity 20s ' +  delay[randomindex] + 's infinite;' +
+                              '-o-animation : animate-opacity 20s ' +  delay[randomindex] + 's infinite;';
         delay.splice(randomindex,1);
       }
       frag.appendChild(block);
